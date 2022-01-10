7d373fa0293ec1cc1c9041103b516b7f6d8efc90
Tommy <82872304+LibertyBear@users.noreply.github.com>
2022-01-10 23:22:27 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+import re
+from pytube import Playlist
+
+DOWNLOAD_DIR = 'C:\\Users\\tomia\\Documents\\PythonYoutubeDownloader\\Downloads\\Downloads'
+global pl 
+pl = "N/A"
+global destination
+destination  = 1
+
+print("Initializing...")
+print("WELCOME BACK MR T")
+
+def showMenu():
+    global pl
+    global destination
+    print("+-----------------------------------+")
+    print("+ Playlist URL:" + pl)
+    print("+ Target Folder (1-3): " + str(destination))
+    print("+-----------------------------------+")
+    print("OPTIONS: setPlaylist, setTarget, execute, exit")
+
+    optn = input()
+    if(optn == "setPlaylist"):
+        setPlaylist()
+    elif(optn == "setTarget"):
+        setTarget()
+    elif(optn == "execute"):
+        execute()
+    elif(optn == "exit"):
+        exit()
+    else:
+        print("Command unclear, please read options (cap sensitive)")
+        showMenu()
+
+
+def setPlaylist():
+    global pl
+    print("Please enter the playlist url:")
+    pl = input()
+    showMenu()
+
+def setTarget():
+    global destination
+    print("Enter your desired download folder (1-3)")
+    destination = input()
+    showMenu()
+
+
+def execute():
+    global pl
+    global destination
+    playlist = Playlist(pl)   
+
+    playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")    
+    print(len(playlist.video_urls))    
+    for url in playlist.video_urls:
+        print(url)    
+    for video in playlist.videos:
+        print('downloading : {} with url : {}'.format(video.title, video.watch_url))
+        video.streams.\
+            filter(type='video', progressive=True, file_extension='mp4').\
+            order_by('resolution').\
+            desc().\
+            first().\
+            download(DOWNLOAD_DIR + destination)
+
+showMenu()
+
+
+
+
+
